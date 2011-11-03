a3e12f680faae6498b73e46d60890c298a0d07c4
dsouza <dsouza@bitforest.org>
2011-11-03 20:27:22 -0200

    improving examples

--- try/htdocs/javascripts/widgets-2.js
+++ try/htdocs/javascripts/widgets-2.js
@@ -1,8 +1,12 @@
 var WIDGETS2 = (function () {
-  jQuery("#widgets-2-link").bind("click", function (e) {
-    var target = jQuery(e.target);
-    alert(target.attr("href"));
-    e.preventDefault();
+  jQuery("#widgets-2").bind("click", function (e) {
+    if (e.target !== undefined) {
+      var target = jQuery(e.target);
+      if (target.attr("id") === "widgets-2-link") {
+        alert(target.attr("href"));
+        e.preventDefault();
+      }
+    }
   });
 
   return({});
